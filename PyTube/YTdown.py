3c15d44e8f58d9c818bccdbebe122b6c1aea58a7
Sawano <swannmerienne@gmail.com>
2022-05-03 14:04:46 +0200

    Update YTdown.py

--- PyTube/YTdown.py
+++ PyTube/YTdown.py
@@ -1,22 +1,29 @@
-from pytube import YouTube  # Make sure to have PyTube installed. If not, copy/paste this line in cmd.
-### pip install pytube3 ###
+from pytube import YouTube
 
-link = input(“Enter the link: “)
+link = input("Enter the link of the video: ")
 yt = YouTube(link)
 
+save_path = ''
 
 def vid_info():
-  print(“Title: “,yt.title) # Title of video
-  print(“Number of views: “,yt.views) # Number of views of video
-  print(“Length of video: “,yt.length,”seconds”)  # Length of the video
-  print("Description: ",yt.description) # Description of video
-  print("Ratings: ",yt.rating)  #Rating
+  print("Title: ",yt.title) # Title of video
+  print("Author: ",yt.author) # author of the video
+  print("Number of views: ",yt.views) # Number of views of video
+  print("Length of video: ",yt.length," seconds")  # Length of the video
   print(yt.streams) # Printing all the available streams
 
-def audiovideo():
-  x = int(input("Type 1 for video downloads, or 2 for audio downloads. >>> "))
-  if x == 2:
-    print(yt.streams.filter(only_audio=True))
-  elif x == 1:
-    print(yt.streams.filter(only_video=True))
+audio = yt.streams.filter(only_audio=True) 
+# audio only, ready to play
 
+video = yt.streams.filter(only_video=True) 
+# has no audio, must download separately, not ready to play
+
+prog = yt.streams.filter(progressive="True") 
+# progressive video has built in audio, ready to play
+
+"""itag = int(input("Enter the itag of the stream you wish to download. "))
+
+stream = yt.streams.get_by_itag(itag)
+stream.download()"""
+
+vid_info()
