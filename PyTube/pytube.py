aa86004c92fff0f943d147a853b0871bba0034fc
Sawano <59657132+SwannMrn@users.noreply.github.com>
2022-05-10 11:54:05 +0200

    Update pytube.py

--- PyTube/pytube.py
+++ PyTube/pytube.py
@@ -2,3 +2,13 @@ from _future_ import unicode_literals
 import io
 import eel
 import youtube_dl
+
+eel.init('web')
+
+def ytdl(data):
+  link = data
+  ydl_opts = {}
+  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([link])
+
+eel.start('index.html',  size=(1000,600)
