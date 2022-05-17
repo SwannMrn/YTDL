ca7425712f9ed64838005fab3fa886f7a4c02f1f
Increible_pollito <69044505+ElPolloTonto750@users.noreply.github.com>
2022-05-17 11:53:05 +0200

    start creating the server for js

--- PyTube/web/main.js
+++ PyTube/web/main.js
@@ -1,1 +1,10 @@
 <script src='script.js'></script>
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.URL-input');
+convertBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURL(URLinput.value);
+});
+function sendURL(URL) {
+
+}
