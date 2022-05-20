9180a57da4bcd83e9fb1fe4a7caacbd635c852a5
Increible_pollito <ventdest@yandex.com>
2022-05-20 09:26:08 +0200

    server works

--- PyTube/web/Server/index.js
+++ PyTube/web/Server/index.js
@@ -3,13 +3,13 @@ const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
 
-# app.use(cors());
+app.use(cors());
 
 app.listen(4000, () => {
     console.log('Server Works !!! At port 4000');
 });
 
-# app.get('/download', (req,res) => {
+app.get('/download', (req,res) => {
     var URL = req.query.URL;
     res.json({url:URL});
 })
--- PyTube/web/main.js
+++ PyTube/web/main.js
@@ -1,4 +1,6 @@
 <script src='script.js'></script>
+
+
 var convertBtn = document.querySelector('.convert-button');
 var URLinput = document.querySelector('.URL-input');
 convertBtn.addEventListener('click', () => {
