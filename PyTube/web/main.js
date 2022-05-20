2210f0ce13648c878032e3c28cb85776079e3f08
Sawano <swannmerienne@gmail.com>
2022-05-20 09:17:52 +0200

    fixed stuff

--- PyTube/web/main.js
+++ PyTube/web/main.js
@@ -1,4 +1,3 @@
-<script src='script.js'></script>
 var convertBtn = document.querySelector('.convert-button');
 var URLinput = document.querySelector('.URL-input');
 convertBtn.addEventListener('click', () => {
