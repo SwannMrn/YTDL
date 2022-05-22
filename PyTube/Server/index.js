f11c55e4cba7bbbd0b1add931e19bb6429d7af88
Sawano <swannmerienne@gmail.com>
2022-05-22 18:15:13 +0200

    Update index.js

--- PyTube/Server/index.js
+++ PyTube/Server/index.js
@@ -9,9 +9,13 @@ app.listen(4000, () => {
     console.log('Server works at port 4000');
 });
 
+app.use(express.static('Server'))
+
 app.get('/download', (req,res) => {
     var URL = req.query.URL;
     res.json({url:URL});
 })
 
-
+app.get('/', function(req,res){
+ res.sendfile(__dirname + '/index.html');
+}); 
