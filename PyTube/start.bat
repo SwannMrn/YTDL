ddf40c77d3d12784c91870f958fcff905c82c5f7
Sawano <swannmerienne@gmail.com>
2022-05-26 22:42:33 +0200

    aaaaaaaa

--- PyTube/start.bat
+++
@@ -1,5 +0,0 @@
-﻿C:\
-cd C:\Documents/Github/PyTube/PyTube/Server
-node index.js
-start http://localhost:4000/
-pause
--- PyTube/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd C:\Github/PyTube/PyTube/Server
-node index.js
-xdg-open http://localhost:4000/
