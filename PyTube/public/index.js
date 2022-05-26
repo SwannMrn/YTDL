7b41de32f89922dcf08354eb371682737e066786
Sawano <swannmerienne@gmail.com>
2022-05-26 18:51:17 +0200

    ye

--- PyTube/public/index.js
+++ PyTube/public/index.js
@@ -4,26 +4,27 @@ const fs = require('fs')
 const ytdl = require('ytdl-core');
 const app = express();
 
+app.use(express.static('public'));
+
 app.use(cors());
 
 app.listen(4000, () => {
     console.log('Server works at port 4000');
 });
 
-app.use(express.static('Server'))
+app.get('/', function(req,res){
+  res.sendFile(__dirname + '/index.html');
+}); 
 
 app.get('/download', (req,res) => {
     var URL = req.query.URL;
     res.json({url:URL});
 })
 
-app.get('/', function(req,res){
- res.sendFile(__dirname + '/index.html');
-}); 
-
-function download() {
+/*function download() {
   ytdl.validateURL(globalVariable.URL)
   ytdl(globalVariable.URL)
     .pipe(fs.createWriteStream('video.mp4'))
   return
 }
+*/
