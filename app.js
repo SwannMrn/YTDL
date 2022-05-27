e5d328d5744b61ce91ae9b18b1b4254d2d959aef
Sawano <swannmerienne@gmail.com>
2022-05-27 14:02:01 +0200

    it almost works

--- app.js
+++ app.js
@@ -14,11 +14,52 @@ app.listen(port, () => console.info(`App listening on port ${port}`))
 // Static Files
 app.use(express.static('public'));
 
-app.get('/', function(req,res){
-  res.sendFile(__dirname + '/public/index.html');
-}); 
+app.get('/downloadmp3', async (req, res, next) => {
+  try {
+    var url = req.query.url;
+    if(!ytdl.validateURL(url)) {
+      return res.sendStatus(400);
+    }
+    let title = 'audio';
 
-app.get('/download', (req,res) => {
-    var URL = req.query.URL;
-    res.json({url:URL});
-})
+    await ytdl.getBasicInfo(url, {
+      format: 'mp4'
+    }, (err, info) => {
+      if (err) throw err;
+      title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+    });
+
+    res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
+    ytdl(url, {
+      format: 'mp3',
+      filter: 'audioonly',
+    }).pipe(res);
+
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get('/downloadmp4', async (req, res, next) => {
+  try {
+    let url = req.query.url;
+    if(!ytdl.validateURL(url)) {
+      return res.sendStatus(400);
+    }
+    let title = 'video';
+
+    await ytdl.getBasicInfo(url, {
+      format: 'mp4'
+    }, (err, info) => {
+      title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+    });
+
+    res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
+    ytdl(url, {
+      format: 'mp4',
+    }).pipe(res);
+
+  } catch (err) {
+    console.error(err);
+  }
+});
