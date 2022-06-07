234bf61286d86b037101e544c9c74235b8e7d306
Sawano <swannmerienne@gmail.com>
2022-06-07 11:48:41 +0200

    added batch files

---
+++ Setup/modules-setup.bat
@@ -0,0 +1,8 @@
+@echo off
+cmd /k npm install -g nodemon
+cmd /k npm install ytdl-core
+cmd /k npm install cors
+cmd /k npm install ejs
+cmd /k npm install express
+cmd /k npm install http
+cmd /k npm install node-fetch
