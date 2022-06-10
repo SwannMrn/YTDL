a3f2dd2bca53aa8f5935f851091052cabfeed5e4
Sawano <59657132+SwannMrn@users.noreply.github.com>
2022-06-10 12:07:48 +0200

    Update modules-setup.bat

--- Setup/modules-setup.bat
+++ Setup/modules-setup.bat
@@ -1,8 +1,8 @@
 @echo off
-cmd /k npm install -g nodemon
-cmd /k npm install ytdl-core
-cmd /k npm install cors
-cmd /k npm install ejs
-cmd /k npm install express
-cmd /k npm install http
-cmd /k npm install node-fetch
+call npm install nodemon
+call npm install ytdl-core
+call npm install cors
+call npm install ejs
+call npm install express
+call npm install http
+call npm install node-fetch
