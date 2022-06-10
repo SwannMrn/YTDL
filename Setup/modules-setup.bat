448ff6b3b667427e42ec188662f2b59934228ea1
Sawano <59657132+SwannMrn@users.noreply.github.com>
2022-06-10 18:49:44 +0200

    Delete Setup directory

--- Setup/modules-setup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call npm install nodemon
-call npm install ytdl-core
-call npm install cors
-call npm install ejs
-call npm install express
-call npm install http
-call npm install node-fetch
